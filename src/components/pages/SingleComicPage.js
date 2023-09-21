0c7b3a9b510be7c1223ff94e5e968a6984c435bf
Maschkov Konstantin <maschkovc@yandex.ru>
2023-09-21 11:55:02 +0300

    Динамические пути

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import AppBanner from '../appBanner/AppBanner';
 
 const SingleComicPage = () => {
 
@@ -37,6 +38,7 @@ const SingleComicPage = () => {
 
     return (
         <>
+            <AppBanner />
             {errorMessage}
             {spinner}
             {content}
@@ -45,12 +47,12 @@ const SingleComicPage = () => {
     )
 }
 
-const View = ({comic}) => {
-    const {title, description, pageCount, thumbnail, language, price} = comic;
+const View = ({ comic }) => {
+    const { title, description, pageCount, thumbnail, language, price } = comic;
 
     return (
         <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
+            <img src={thumbnail} alt={title} className="single-comic__img" />
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
                 <p className="single-comic__descr">{description}</p>
